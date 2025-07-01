0cbfdb76e4b36f3ce95b1dccf89c8cbd5f10fdb7
Batoolserag <batoolserag@gmail.com>
2025-07-01 11:43:22 +0400

    Delete discussions.js

--- discussions.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const discussions = require('../data/discussions');
-
-router.get('/', (req, res) => {
-    res.json(discussions);
-});
-
-module.exports = router;
