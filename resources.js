c1f67c6ea901549f07806098ab7df2e2bddcbbaa
Batoolserag <batoolserag@gmail.com>
2025-07-01 11:43:39 +0400

    Delete resources.js

--- resources.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const resources = require('../data/resources');
-
-router.get('/', (req, res) => {
-    res.json(resources);
-});
-
-module.exports = router;
