8927fff3323c542261f3d714c2b9652a36882acd
Batoolserag <batoolserag@gmail.com>
2025-07-01 11:43:12 +0400

    Delete activities.js

--- activities.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const activities = require('../data/activities');
-
-router.get('/', (req, res) => {
-    res.json(activities);
-});
-
-module.exports = router;
